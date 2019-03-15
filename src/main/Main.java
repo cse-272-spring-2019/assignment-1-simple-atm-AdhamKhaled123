a4270105bd5227d17705e84cc04affa945b85155
Super_Star <Super_Star@star>
2019-03-15 23:40:12 +0200

    Project1

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+
+package main;
+
+
+
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        MyGUI n=new MyGUI();
+        n.setVisible(true);  //to make the screen appear
+        n.setSize(650,450);  // control length and width of the screen
+    }
+
+   
+    
+}
